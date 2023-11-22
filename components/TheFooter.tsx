badaff16688c3cdaaf5d10bd626d6c8178c18950
BatanAndrei <yabatan777@gmail.com>
2023-11-22 15:17:28 +0300

    initial email commit

--- components/TheFooter.tsx
+++ components/TheFooter.tsx
@@ -1,9 +1,11 @@
 import style from './TheFooter.module.css';
+import { FOOTER_EMAIL } from "@/const";
 
 export default function TheFooter ()  {
     return (
         <footer className={style.footer}>
-            &copy; 2023 Web studio
+            <div>&copy; 2023 Web studio</div>
+            <a href={`mailto:${FOOTER_EMAIL}`}>{FOOTER_EMAIL}</a>
         </footer>
     )
 }
