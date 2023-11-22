f2b794c0b3736d3e3049d872c9ac846f670826ba
BatanAndrei <yabatan777@gmail.com>
2023-11-22 15:01:22 +0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 
 export default function Home() {
   return (
-    <h1>Главная</h1>
+    <>
+        <h1>Главная</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam voluptas ad nobis. Molestias illum perspiciatis iure ut necessitatibus accusamus impedit obcaecati velit quod iste exercitationem, suscipit dicta ab et repellendus.</p>
+    </>
   )
 }
